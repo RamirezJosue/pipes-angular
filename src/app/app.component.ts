5cfe82afc449a12e6fad5848351f430d0173e6ac
RamirezJosue <xdemobtkx@gmail.com>
2018-03-18 14:44:59 -0600

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Josue";
+  nombre2 = "josue raMirez yugRa"
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe={
+    nombre:"Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion:{
+      calle:"Pimera",
+      casa: "19"
+    }
+  }
+
+  valorDePromesa = new Promise( (resolve,reject)=> {
+    setTimeout(()=>resolve('Llego la data!'),3500 );
+  });
+
+  fecha = new Date();
+
+  video = "mOeSfOJrUIk"
 }
